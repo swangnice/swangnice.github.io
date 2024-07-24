a4b312d09a703e42e8eb2147e4dfa53d1266af52
swangnice <swangnice@outlook.com>
2024-07-24 04:52:38 -0400

    add pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     alert('欢迎访问我的博客！');
-// });
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+});
+// blog jump
+document.addEventListener('DOMContentLoaded', function() {
+    var accordionItems = document.querySelectorAll('.accordion_child0');
+    accordionItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      var url = this.getAttribute('data-url');
+      window.location.href = url;
+    });
+  });
+});
+// project card jump
+document.addEventListener('DOMContentLoaded', function() {
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(function(card) {
+    card.addEventListener('click', function() {
+      var url = this.getAttribute('data-url');
+      window.location.href = url;
+    });
+  });
+});
+
+
+// document.addEventListener('DOMContentLoaded', function() {/* add things */});
